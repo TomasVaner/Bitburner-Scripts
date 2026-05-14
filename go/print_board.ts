8e7143d5342877d020469ba94ce6904791274b16
Tomas Vaner <TomasVanerG@gmail.com>
2026-05-14 14:28:00 +0300

    go: solver

---
+++ go/print_board.ts
@@ -0,0 +1,28 @@
+export async function main(ns: NS) {
+  const board = ns.go.getBoardState();
+  ns.clearLog();
+  const size = board.length;
+
+  for (let y = size-1; y >= 0; --y)
+  {
+    let row = ""
+    for (let x = 0; x < size; ++x) 
+    {
+      row += board[x][y];
+    }
+    ns.print(row)
+  }
+  board.copyWithin
+  ns.print('----------');
+  let chains = ns.go.analysis.getChains();
+  for (let y = size-1; y >= 0; --y)
+  {
+    let row = ""
+    for (let x = 0; x < size; ++x) 
+    {
+      row += (chains[x][y] ?? " ---").toString().padStart(4);
+    }
+    ns.print(row)
+  }
+
+}
